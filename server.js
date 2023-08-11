6117b596bb5beb86413fc5c28cb7f74e0ae276bc
Kyrylo <fresh.js.ftd.@gmail.com>
2023-08-11 18:10:05 +0300

    Fix server route

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get((req, res, next) => {
-  res.send('Hello from container');
+app.get('/', (req, res, next) => {
+  res.send('Magic');
 });
 
 const PORT = process.env.PORT || 5000;
